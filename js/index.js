c62bab604ca4fd755ffb8695f727e2ed6706861f
amichalo421 <andre.michalowski@gmail.com>
2019-10-29 11:54:42 +0100

    halfCompleted

--- js/index.js
+++ js/index.js
@@ -1,84 +1,90 @@
 // --------- QuerySelectors and Event's --------------
 
-// * [ ] `mouseover`
-    const firstName = document.querySelector('.first-name');
-    console.log(firstName);
-        firstName.addEventListener('mouseover', (e) => {
-            console.log(e.target.value);
-            alert('Hello!');
+// * [ ] `click`
+    const home = document.querySelectorAll('a')[0];
+        console.log(home);
+            home.addEventListener('click', event => {
+                console.log(`Input detected ${event.target}`);   
+                alert('click'); 
+            });
+
+// // * [ ] `mouseover`
+    const about = document.querySelectorAll('a')[1];
+    console.log(about);
+        about.addEventListener('mouseover', event => {
+            console.log(`Input detected for about`);   
+            alert('mouseover'); 
         });
 
-// * [ ] `keydown`
-    const navLink = document.querySelector('.nav-link');
-    console.log(navLink);
-        firstName.addEventListener('keydown', (e) => {
-            console.log(e.target.value);
-            alert('Hello!');
+// // * [ ] `keydown`
+    const blog = document.querySelectorAll('a')[2];
+    console.log(blog);
+        blog.addEventListener('keydown', event => {
+            console.log(`Input detected for keydown ${event.target}`);   
+            alert('keydown'); 
         });
 
-// * [ ] `wheel`
-    const button = document.querySelector('button');
-    console.log(navLink);
-        firstName.addEventListener('wheel', (e) => {
-            console.log(e.target.value);
-            alert('Hello!');
-        });
+// // * [ ] `wheel`
+    const contact = document.querySelectorAll('a')[3];
+        console.log(contact);
+            contact.addEventListener('wheel', event => {
+                console.log(`Input detected for ${event.target}`);   
+                alert('wheel'); 
+            });
 
-// * [ ] `drag / drop`
-    const create = document.querySelector('.form-submit');
-    console.log(navLink);
-        firstName.addEventListener('drag / drop', (e) => {
-            console.log(e.target.value);
-            alert('Hello!');
-        });
+// // * [ ] `drag / drop`
+//     const create = document.querySelector('.form-submit');
+//     console.log(navLink);
+//         firstName.addEventListener('drag / drop', (e) => {
+//             console.log(e.target.value);
+//             alert('Hello!');
+//         });
 
-// * [ ] `load`
-    const button = document.querySelector('button');
-    console.log(navLink);
-        firstName.addEventListener('load', (e) => {
-            console.log(e.target.value);
-            alert('Hello!');
-        });
+// // * [ ] `load`
+//     const button = document.querySelector('button');
+//     console.log(navLink);
+//         firstName.addEventListener('load', (e) => {
+//             console.log(e.target.value);
+//             alert('Hello!');
+//         });
     
-// * [ ] `focus`
-    const button = document.querySelector('button');
-    console.log(navLink);
-        firstName.addEventListener('focus', (e) => {
-            console.log(e.target.value);
-            alert('Hello!');
+// // * [ ] `focus`
+//     const button = document.querySelector('button');
+//     console.log(navLink);
+//         firstName.addEventListener('focus', (e) => {
+//             console.log(e.target.value);
+//             alert('Hello!');
+//         });
+
+// // * [ ] `resize`
+//     const button = document.querySelector('button');
+//     console.log(navLink);
+//         firstName.addEventListener('resize', (e) => {
+//             console.log(e.target.value);
+//             alert('Hello!');
+//         });
+
+// // * [ ] `scroll`
+//     const funBus = document.querySelectorAll('img')[0];
+        funBus.addEventListener('dblclick', event => {
+            console.log(`Input detected ${event.target}`);   
+            alert('dblclick the secret funbus'); 
         });
 
-// * [ ] `resize`
-    const button = document.querySelector('button');
-    console.log(navLink);
-        firstName.addEventListener('resize', (e) => {
-            console.log(e.target.value);
-            alert('Hello!');
-        });
+// // * [ ] `select`
+//     const button = document.querySelector('button');
+//     console.log(navLink);
+//         firstName.addEventListener('select', (e) => {
+//             console.log(e.target.value);
+//             alert('Hello!');
+//         });
 
-// * [ ] `scroll`
-    const button = document.querySelector('button');
-    console.log(navLink);
-        firstName.addEventListener('scroll', (e) => {
-            console.log(e.target.value);
-            alert('Hello!');
+// // * [ ] `dblclick
+    const funBus = document.querySelectorAll('img')[0];
+        funBus.addEventListener('dblclick', event => {
+            console.log(`Input detected ${event.target}`);   
+            alert('dblclick the secret funbus'); 
         });
-
-// * [ ] `select`
-    const button = document.querySelector('button');
-    console.log(navLink);
-        firstName.addEventListener('select', (e) => {
-            console.log(e.target.value);
-            alert('Hello!');
-        });
-
-// * [ ] `dblclick`
-    const button = document.querySelector('button');
-    console.log(navLink);
-      trade.addEventListener('dblclick', (e) => {
-        e.target.style.backgroundColor = 'blue';
-        e.stopPropagation();
-      });
 
 
 
@@ -134,3 +140,37 @@
 // someFunc() {
 //   console.log('clicked button');
 // }
+
+// const button = document.querySelector(".lambda-button").addEventListener('click', (event) => {
+// 	console.log(event);
+// 	const rand = Math.floor(Math.random() * 100);
+// ​
+// 	// Easter Egg :)
+// 	if (rand === 42 || rand === 31) {
+// 		setInterval(() => {
+// 			document.body.style.backgroundColor = getRandomColor();
+// 		}, 100);
+// 	}
+// });
+// ​
+// getRandomColor = () => {
+// 	var letters = '0123456789ABCDEF';
+// 	var color = '#';
+// 	for (var i = 0; i < 6; i++) {
+// 		color += letters[Math.floor(Math.random() * 16)];
+// 	}
+// 	return color;
+// }
+
+//----LAST NAME INPUT BOX FROM LECTURE
+
+// const lastName = document.getElementsByName('lastname')[0];
+// lastName.addEventListener('keydown', event=>{
+//     console.log(`Input detected ${event.key}`);    
+// });
+
+// const lastNameInput = document.querySelector('input[name="lastname"]');
+// ​
+// lastNameInput.addEventListener('keydown', (event) => {
+// 	console.log(`Input detected keypress: ${event.key}`);
+// });
